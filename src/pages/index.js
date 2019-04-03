a24a7f0186e26ff4c72e2f0e23a67a700d33d7ee
Yuxiang Dai <yuxiang.dai43@gmail.com>
2019-04-02 22:28:32 -0400

    Add initial two-column layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
 import Image from '../components/image'
-import logo from '../images/Yuxiang_Signature_White2.png'
+import logo from '../images/Yuxiang_Signature_White.png'
 import styles from '../styles/index.module.css'
 
 const IndexPage = () => (
@@ -11,28 +11,11 @@ const IndexPage = () => (
     <img className={styles.headerImage} src={logo} alt="Yuxiang Dai" />
     <h1>about</h1>
     <p>I am Yuxiang Dai</p>
-    <p>Now go build something great.</p>
-
-    <p>
-      Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-      Lorem Ipsum has been the industry's standard dummy text ever since the
-      1500s, when an unknown printer took a galley of type and scrambled it to
-      make a type specimen book. Lorem Ipsum is simply dummy text of the
-      printing and typesetting industry. Lorem Ipsum has been the industry's
-      standard dummy text ever since the 1500s, when an unknown printer took a
-      galley of type and scrambled it to make a type specimen book. Lorem Ipsum
-      is simply dummy text of the printing and typesetting industry. Lorem Ipsum
-      has been the industry's standard dummy text ever since the 1500s, when an
-      unknown printer took a galley of type and scrambled it to make a type
-      specimen book. Lorem Ipsum is simply dummy text of the printing and
-      typesetting industry. Lorem Ipsum has been the industry's standard dummy
-      text ever since the 1500s, when an unknown printer took a galley of type
-      and scrambled it to make a type specimen book.
-    </p>
+    <p>This site is a work in progress</p>
     <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
       {/* <Image /> */}
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Link to="/projects/">Go to my projects</Link>
   </Layout>
 )
 
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -2,11 +2,29 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
+import styles from '../styles/index.module.css'
 
 const SecondPage = () => (
   <Layout>
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
+    <h1>projects</h1>
+    <div class="container">
+      <div className={styles.row}>
+        <div className={styles.projectItem}>
+          <p>test</p>
+        </div>
+        <div className={styles.projectItem}>
+          <p>test</p>
+        </div>
+      </div>
+      <div className={styles.row}>
+        <div className={styles.projectItem}>
+          <p>test</p>
+        </div>
+        <div className={styles.projectItem}>
+          <p>test</p>
+        </div>
+      </div>
+    </div>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
