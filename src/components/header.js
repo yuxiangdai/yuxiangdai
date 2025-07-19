2c3eb13ce0a162fb4e0d4f182bdc7ea3f5e371d0
Yuxiang Dai <yuxiang.dai43@gmail.com>
2025-07-19 14:20:19 -0400

    Fix CSS module imports and add CLAUDE.md
    - Fix CSS module imports in header.js, photos.js, and projects.js to use namespace imports
    - Add CLAUDE.md with development commands and architecture overview

--- src/components/header.js
+++ src/components/header.js
@@ -2,7 +2,7 @@ import { Link } from 'gatsby'
 import styled from 'styled-components'
 import PropTypes from 'prop-types'
 import React from 'react'
-import styles from './header.module.css'
+import * as styles from './header.module.css'
 
 const Container = styled.div`
   background: black;
