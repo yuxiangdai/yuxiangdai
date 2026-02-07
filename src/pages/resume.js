ab249db341b2659268cbbe745e2106f741dea24a
Yuxiang Dai <yuxiang.dai43@gmail.com>
2026-02-07 12:21:49 -0800

    Hide llms endpoint from resume UI and README (#23)

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,13 +1,12 @@
 import React from 'react'
-import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
-import Image from '../components/image'
 
 const ResumePage = () => (
   <Layout>
     <h1>resume</h1>
     <iframe
+      title="Resume PDF"
       src="https://drive.google.com/file/d/1pLyV-JBoSXRU-M6xu7SDHmrDfegQ0Ed2/preview"
       width="80%"
       style={{
