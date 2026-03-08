dcaf6832a4af6bb66d5625d2e522e31e84e34fb4
Yuxiang Dai <yuxiang.dai43@gmail.com>
2026-03-08 08:12:50 -0700

    Refresh eslint config settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,61 +1,28 @@
 module.exports = {
-  extends: ['standard'],
-  plugins: ['standard', 'react'],
-  rules: {
-    'no-var': 'error', // optional, recommended when using es6+
-    'no-unused-vars': 1, // recommended
-    'arrow-spacing': [
-      'error',
-      {
-        before: true,
-        after: true,
-      },
-    ], // recommended
-    indent: ['error', 2],
-    'comma-dangle': [
-      'error',
-      {
-        objects: 'only-multiline',
-        arrays: 'only-multiline',
-        imports: 'never',
-        exports: 'never',
-        functions: 'never',
-      },
-    ],
-
-    // options to emulate prettier setup
-    semi: ['error', 'never'],
-    'max-len': [
-      'error',
-      {
-        code: 80,
-      },
-    ],
-    'template-curly-spacing': ['error', 'always'],
-    'arrow-parens': ['error', 'as-needed'],
-
-    // standard.js
-    'space-before-function-paren': [
-      'error',
-      {
-        named: 'always',
-        anonymous: 'always',
-        asyncArrow: 'always',
-      },
-    ],
-
-    // standard plugin - options
-    'standard/object-curly-even-spacing': ['error', 'either'],
-    'standard/array-bracket-even-spacing': ['error', 'either'],
-    'standard/computed-property-even-spacing': ['error', 'even'],
-    'standard/no-callback-literal': ['error', ['cb', 'callback']],
-
-    // react plugin - options
-    'react/jsx-uses-react': 'error',
-    'react/jsx-uses-vars': 'error',
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
   },
-  parser: 'babel-eslint',
   parserOptions: {
-    ecmaVersion: 8, // optional, recommended 6+
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  plugins: ['react'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/no-unescaped-entities': 'off',
+    'no-unused-vars': 'warn',
   },
 }
